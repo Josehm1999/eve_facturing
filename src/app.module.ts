83831f3e7e130f0b5041b878b5c8450fdbb5bf14
JoseH <josehm1999@gmail.com>
2023-01-15 00:05:53 -0500

    Add: configuration added to work with environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
 	imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env'
+        }),
 		UsersModule,
 		TypeOrmModule.forRoot({
 			type: 'mysql',
-			host: 'localhost',
-			port: 3306,
-			username: 'root',
-			password: 'Jjoossee9.9',
+			host: process.env.DEV_DB_HOST,
+			port: parseInt(process.env.DEV_DB_PORT, 10) || 3306,
+			username: process.env.DEV_DB_USER,
+			password: process.env.DEV_DB_PASSWORD,
 			database: 'eve_facturing',
 			entities: [__dirname + '/**/*.entity{.ts,.js}'],
             synchronize: true
